9195875f35c5362b0eb1e62259f3e5ee2f0e85f4
George Kaimakis <geomatus@mac.com>
2017-03-01 11:43:46 +0000

    Adds variables for color values:

--- neopix.py
+++ neopix.py
@@ -7,9 +7,13 @@ import sys
 #print(sys.arg[1])
 #print(sys.arg[2])
 
+red     = 128
+green   = 0
+blue    = 128
+
 pin = Pin(0, Pin.OUT)
 #pin.high()
 np = NeoPixel(pin, 1)
 
-np[0] = (55, 200, 250)
+np[0] = (red, green, blue)
 np.write()
